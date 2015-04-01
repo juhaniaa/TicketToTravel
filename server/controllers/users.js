b7a8583c91f12d94908b8f090287faf6fbfaccf5
Juhani Aavanen <ja222qm@student.lnu.se>
2015-04-01 18:29:19 +0300

    Testing mongo err with heroku

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,25 +2,29 @@ var User = require('mongoose').model('User'),
   encrypt = require('../utilities/encryption');
 
 exports.createUser = function(req, res, next) {
-  
-  res.render('../../public/app/test');
-  // var userData = req.body;
-  // userData.username = userData.username.toLowerCase();
-  // userData.salt = encrypt.createSalt();
-  // userData.hashed_pwd = encrypt.hashPwd(userData.salt, userData.password);
-  // User.create(userData, function(err, user) {
-  //   if(err) {
-  //     if(err.toString().indexOf('E11000') > -1) {
-  //       err = new Error('Duplicate Username');
-  //     }
-  //     res.status(400);
-  //     return res.send({reason:err.toString()});
-  //   }
-  //   req.logIn(user, function(err) {
-  //     if(err) {return next(err);}
-  //     res.send(user);
-  //   })
-  // })
+
+  var userData = req.body;
+  userData.username = userData.username.toLowerCase();
+  userData.salt = encrypt.createSalt();
+  userData.hashed_pwd = encrypt.hashPwd(userData.salt, userData.password);
+  User.create(userData, function(err, user) {
+    if(err) {
+      if(err.toString().indexOf('E11000') > -1) {
+        err = new Error('Duplicate Username');
+      }
+      res.status(400);
+      return res.send({reason:err.toString()});
+    }
+    req.logIn(user, function(err) {
+      if(err) {
+        err = new Error('Some crazy shit going on');
+        res.status(400);
+        return res.send({reason:err.toString()});
+        // return next(err);
+      }
+      res.send(user);
+    })
+  })
 }
 
 exports.updateUser = function(req, res) {
