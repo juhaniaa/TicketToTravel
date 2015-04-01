95eb765557c5cc08a272472df4786bb5f16c56c9
Juhani Aavanen <ja222qm@student.lnu.se>
2015-04-01 18:54:45 +0300

    Trying new dbuser

--- server/config/config.js
+++ server/config/config.js
@@ -3,12 +3,13 @@ var rootPath = path.normalize(__dirname + '/../../');
 
 module.exports = {
   development: {
-    db: 'mongodb://localhost/tickettravel',
+    // db: 'mongodb://localhost/tickettravel',
+    db: 'mongodb://travelapp:qwetravel@ds039211.mongolab.com:39211/tickettravel',
     rootPath: rootPath,
     port: process.env.PORT || 3030
   },
-  production: {    
-    db: 'mongodb://traveler:123qweasd@ds039211.mongolab.com:39211/tickettravel',
+  production: {
+    db: 'mongodb://travelapp:qwetravel@ds039211.mongolab.com:39211/tickettravel',
     rootPath: rootPath,
     port: process.env.PORT || 80
   }
