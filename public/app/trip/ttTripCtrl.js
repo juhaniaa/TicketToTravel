e6a328ba4d8e1ffece1acf8667a08dcd6434ba33
Juhani Aavanen <ja222qm@student.lnu.se>
2015-04-15 12:53:40 +0300

    Added dropdowns for trip options

--- public/app/trip/ttTripCtrl.js
+++ public/app/trip/ttTripCtrl.js
@@ -1,5 +1,24 @@
 angular.module('app').controller('ttTripCtrl', ['$scope', function($scope) {
-  // $scope.purchase = function() {
-  //   // let user put in the payment info
-  // }
+
+  // first user selects a from station from all available
+  // then the to-station is allowed to be chosen from list of stations on same route
+  //...what if multiple routes on the from-station?
+  var stations = [
+    { id: 1, name: 'Gottby', nr: 15, position: 'x' },
+    { id: 2, name: 'Mariehamn', nr: 1, position: 'x' },
+    { id: 3, name: 'Eckerö', nr: 25, position: 'x' },
+    { id: 4, name: 'Godby', nr: 11, position: 'x' }
+  ];
+
+  var routes = [
+    { id: 1, name: 'Eckerö-linjen', nr: 3, stops: [stations[0], stations[1], stations[2]]},
+    { id: 1, name: 'Godby-linjen', nr: 3, stops: [stations[0], stations[3]]}
+  ];
+
+  $scope.stations = stations;
+  $scope.routes = routes;
+  $scope.trip = {
+    origin: stations[1],
+    destination: stations[3]
+  };
 }])
